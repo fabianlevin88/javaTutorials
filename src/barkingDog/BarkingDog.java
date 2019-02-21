d634d9bee61faa363e208ee4b1503fdd25800b79
Fabian Andres Levin <fabianlevin60@gmail.com>
2019-02-21 16:05:02 -0300

    Barking dog exercise added

---
+++ src/barkingDog/BarkingDog.java
@@ -0,0 +1,14 @@
+package barkingDog;
+
+public class BarkingDog {
+
+    public static void shouldWakeUp(boolean barking, int hourOfDay) {
+        if (hourOfDay < 0 || hourOfDay > 23) {
+            System.out.println(false);
+        } else if (barking && (hourOfDay < 8 || hourOfDay > 22)) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+    }
+}
