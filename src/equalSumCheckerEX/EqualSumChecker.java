489eceabeb5b8c1be63ba0fde7098c7debb06e4c
fabitolevin88 <fabianlevin60@gmail.com>
2019-07-03 20:49:34 -0300

    Added some overloaded methods just for fun

--- src/equalSumCheckerEX/EqualSumChecker.java
+++ src/equalSumCheckerEX/EqualSumChecker.java
@@ -11,4 +11,37 @@ public class EqualSumChecker {
         System.out.println("False");
         return false;
     }
+
+    /**
+     * Overloading method exercise just for fun
+     * @param numA
+     * @param numB
+     * @return
+     */
+    public static int sum(int numA, int numB) {
+
+        return numA + numB;
+    }
+
+    /**
+     * Method overload, just changing the type of parameters received
+     * @param numA
+     * @param numB
+     * @return
+     */
+    public static int sum(double numA, double numB) {
+
+        return (int) (numA + numB);
+    }
+
+    /**
+     * Overloading by changing the number of parameters
+     * @param numA
+     * @param numB
+     * @param numC
+     * @return
+     */
+    public static int sum(int numA, int numB, int numC) {
+        return numA + numB + numC;
+    }
 }
--- src/equalSumCheckerEX/Main.java
+++ src/equalSumCheckerEX/Main.java
@@ -6,5 +6,16 @@ public class Main {
         EqualSumChecker sumChecker = new EqualSumChecker();
 
         sumChecker.hasEqualSum(6,-2,4);
+
+        // Method overloading
+        int number = sumChecker.sum(4, 9);
+
+        int numberTwo = sumChecker.sum(2, -7);
+
+        int numberThree = sumChecker.sum(9, -6, 2);
+
+        System.out.println("Calling first sum: " + number + " and second call: " + numberTwo);
+
+        System.out.println("Calling last method with 3 parameters: " + numberThree);
     }
 }
