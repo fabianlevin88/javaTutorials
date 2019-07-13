6d2b4ed574d13be97e9cae4cedf1ffb326e7b881
fabitolevin88 <fabianlevin60@gmail.com>
2019-07-13 12:48:17 -0300

    Playing cat exercise completed

---
+++ src/playingCatEX/Main.java
@@ -0,0 +1,12 @@
+package playingCatEX;
+
+import static playingCatEX.PlayingCat.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(isCatPlaying(true, 10));
+        System.out.println(isCatPlaying(false, 36));
+        System.out.println(isCatPlaying(false, 35));
+    }
+}
