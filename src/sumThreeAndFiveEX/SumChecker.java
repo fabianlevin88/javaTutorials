9649d9fc9579e1926b019dbf90e67b47f3d40f61
fabitolevin88 <fabianlevin60@gmail.com>
2019-07-21 13:05:35 -0300

    sum 3 and 5 exercise completed

---
+++ src/sumThreeAndFiveEX/SumChecker.java
@@ -0,0 +1,22 @@
+package sumThreeAndFiveEX;
+
+public class SumChecker {
+
+    public static void checkSum() {
+
+        int count = 0;
+        int sum = 0;
+
+        for (int i=1; i <= 1000; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                count++;
+                sum += i;
+                System.out.println(i);
+            }
+            if (count == 5) {
+                break;
+            }
+        }
+        System.out.println("Total sum is: " + sum);
+    }
+}
