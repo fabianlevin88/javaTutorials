080c0d6cc4bcf82a7009717c85d15a7d26b97e92
fabitolevin88 <fabianlevin60@gmail.com>
2019-07-29 17:52:52 -0300

    First and last digit sum exercise completed

---
+++ src/firstLastDigitEX/FirstLastDigitSum.java
@@ -0,0 +1,21 @@
+package firstLastDigitEX;
+
+public class FirstLastDigitSum {
+
+    public static int sumFirstAndLastDigit(int number) {
+
+        if (number < 0) {
+            return -1;
+        }
+
+        int lastDigit = number % 10;
+
+        int firstDigit = number;
+
+        while (firstDigit >= 10) {
+            firstDigit /= 10;
+        }
+
+        return firstDigit + lastDigit;
+    }
+}
---
+++ src/firstLastDigitEX/Main.java
@@ -0,0 +1,14 @@
+package firstLastDigitEX;
+
+import static firstLastDigitEX.FirstLastDigitSum.sumFirstAndLastDigit;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(sumFirstAndLastDigit(252));
+        System.out.println(sumFirstAndLastDigit(257));
+        System.out.println(sumFirstAndLastDigit(0));
+        System.out.println(sumFirstAndLastDigit(5));
+        System.out.println(sumFirstAndLastDigit(-10));
+    }
+}
