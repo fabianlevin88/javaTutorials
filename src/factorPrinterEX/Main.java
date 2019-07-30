4544172379d210ffeb704c23543b6b533eaa1c1b
fabitolevin88 <fabianlevin60@gmail.com>
2019-07-30 19:13:22 -0300

    factor printer exercise completed

---
+++ src/factorPrinterEX/Main.java
@@ -0,0 +1,19 @@
+package factorPrinterEX;
+
+import static factorPrinterEX.FactorPrinter.printFactors;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("********************");
+        printFactors(6);
+        System.out.println("********************");
+        printFactors(32);
+        System.out.println("********************");
+        printFactors(10);
+        System.out.println("********************");
+        printFactors(-1);
+        System.out.println("********************");
+    }
+
+}
