866fbac89a96cc685d1291654bac01f77d38df0f
fabitolevin88 <fabianlevin60@gmail.com>
2019-08-01 21:56:57 -0300

    number to words V2 exercise completed

---
+++ src/numberToWordsV2EX/Main.java
@@ -0,0 +1,17 @@
+package numberToWordsV2EX;
+
+import static numberToWordsV2EX.NumberToWords.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        numberToWords(123);
+        System.out.println();
+        numberToWords(1010);
+        System.out.println();
+        numberToWords(1000);
+        System.out.println();
+        numberToWords(-12);
+    }
+
+}
