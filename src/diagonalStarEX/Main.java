491789ea203fa546673e3d640a7e8e0efec070cb
fabitolevin88 <fabianlevin60@gmail.com>
2019-08-04 13:51:48 -0300

    Diagonal star exercise completed

---
+++ src/diagonalStarEX/Main.java
@@ -0,0 +1,13 @@
+package diagonalStarEX;
+
+import static diagonalStarEX.DiagonalStar.printSquareStar;
+
+public class Main {
+
+    public static void main(String[] args) {
+        printSquareStar(4);
+        printSquareStar(5);
+        printSquareStar(10);
+    }
+
+}
