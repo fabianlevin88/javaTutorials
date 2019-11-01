4905208bd7ca786c98c3bd74adede47e9089ae19
fabitolevin88 <fabianlevin60@gmail.com>
2019-11-01 19:50:13 -0300

    inheritance exercise completed

---
+++ src/inheritanceEX/Main.java
@@ -0,0 +1,13 @@
+package inheritanceEX;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+	    Outlander outlander = new Outlander(36);
+
+	    outlander.steer(45);
+	    outlander.accelerate(50);
+	    outlander.accelerate(-20);
+    }
+}
